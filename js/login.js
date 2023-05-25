4bbaa2d124eb3de3bfbed27dd28e52a65dbd8edb
Gabriela Marsiglia <rm551237@gmail.com>
2023-05-25 15:15:13 -0300

    criado sucesso.js.

--- js/login.js
+++ js/login.js
@@ -1,3 +1,4 @@
+
 //GERANDO DOIS OBJETOS NO LOCALSTORAGE
 const usuario1 = {
     nomeUsuario:"predo",
@@ -39,23 +40,76 @@ addEventListener("click",(evento)=>{
         const h1Titulo = document.querySelector("#titulo");
 
         let lista = JSON.parse(localStorage.getItem("listaUser"));
-        const BreakError = {};
+
+
+        let userValidado = {
+   
+        };
+       
         try{
             lista.forEach((usuario)=> {
                 //VALIDAÇÃO
                 if(inputUserValue == usuario.nomeUsuario && inputPassValue == usuario.senhaUsuario){
-                h1Titulo.innerHTML = "Bem vindo : " + usuario.nomeUsuario;
-                throw "VALIDADO";
-                }else{ 
-                    throw "NÃO VALIDADO";
+                    userValidado = usuario;
+                    throw "VALIDADO";
                 }
             });
-        }catch(err){
-            if(err == "VALIDADO"){
-                console.log("VALIDADO");
+
+            //Caso não ocorra validação o throw é lançado para o catch com a string referente.
+            throw "NÃO VALIDADO";
+
+        }catch(msg){
+            if(msg == "VALIDADO"){
+
+                //Criando o objeto userValidado no LocalStorage
+                localStorage.setItem("user-validado",  JSON.stringify(userValidado));
+                
+                h1Titulo.innerHTML = "<span><strong>Login validado com sucesso!</strong></span>";
+                h1Titulo.setAttribute("style","color:#00ff00;");
+
+                //Adicionando uma propriedade ao nosso objeto userValidado
+                let token = Math.random().toString(16).substring(2)+Math.random().toString(16).substring(2);
+
+                //Atualizando o token no LocalStorage
+                localStorage.setItem("user-token",  JSON.stringify(token));
+
+                //Direcionando o usuário para a página de sucesso!
+                window.location.href = "../sucesso.html";
+
             }else{
-                console.log("NÃO VALIDADO");
+                h1Titulo.innerHTML = "<span><strong>Login ou senha inválidos!</strong></span>";
+                h1Titulo.setAttribute("style","color:#ff0000;");
+                window.location.href = "../erro.html";
             }
         }       
     }
 });
+
+// try{
+    
+//     if(JSON.parse(localStorage.getItem("user-token")) != null){
+//         const userBemVindo = document.querySelector("#userWelcome");
+//         let usuario = JSON.parse(localStorage.getItem("user-validado"));
+        
+//         userBemVindo.innerHTML = usuario.nomeUsuario;
+//         const botaoLogout = document.querySelector("#btnLogout");
+        
+//         botaoLogout.addEventListener("click", ()=>{
+//             localStorage.removeItem("user-validado");
+//             window.location.href = "../login.html";
+//         });
+//     }else{
+//         throw null;
+//     }
+
+// }catch(erro){
+
+//     if(userBemVindo != null){
+//         userBemVindo.innerHTML = JSON.parse(localStorage.getItem("user-validado")).nomeUsuario;
+//     }else{
+//         window.location.href = "../login.html";
+//     }
+// }
+
+
+
